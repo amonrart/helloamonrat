b508314792c7da8a0a14af44796d1d0a88dfb4c1
amonrart <amonrart.m@kkumail.com>
2022-02-18 20:34:40 +0700

    Student Management

--- Student Management/Student Management/Form1.Designer.cs
+++ Student Management/Student Management/Form1.Designer.cs
@@ -36,15 +36,25 @@
             this.ColumnID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnMajor = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxid = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBoxname = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxMajor = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxGpa = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxMax = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxGpax = new System.Windows.Forms.TextBox();
             this.menuStrip2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip2
@@ -82,7 +92,6 @@
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
@@ -91,10 +100,10 @@
             this.ColumnID,
             this.ColumnName,
             this.ColumnMajor});
-            this.dataGridView1.Location = new System.Drawing.Point(237, 46);
+            this.dataGridView1.Location = new System.Drawing.Point(229, 46);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(452, 209);
+            this.dataGridView1.Size = new System.Drawing.Size(460, 326);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -113,16 +122,16 @@
             this.ColumnMajor.HeaderText = "Major";
             this.ColumnMajor.Name = "ColumnMajor";
             // 
-            // textBox2
+            // textBoxid
             // 
-            this.textBox2.Location = new System.Drawing.Point(63, 46);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(121, 23);
-            this.textBox2.TabIndex = 3;
+            this.textBoxid.Location = new System.Drawing.Point(63, 46);
+            this.textBoxid.Name = "textBoxid";
+            this.textBoxid.Size = new System.Drawing.Size(121, 23);
+            this.textBoxid.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(74, 139);
+            this.button1.Location = new System.Drawing.Point(63, 168);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 4;
@@ -148,12 +157,12 @@
             this.label2.TabIndex = 7;
             this.label2.Text = "Name";
             // 
-            // textBox3
+            // textBoxname
             // 
-            this.textBox3.Location = new System.Drawing.Point(63, 75);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(121, 23);
-            this.textBox3.TabIndex = 6;
+            this.textBoxname.Location = new System.Drawing.Point(63, 75);
+            this.textBoxname.Name = "textBoxname";
+            this.textBoxname.Size = new System.Drawing.Size(121, 23);
+            this.textBoxname.TabIndex = 6;
             // 
             // label3
             // 
@@ -164,30 +173,112 @@
             this.label3.TabIndex = 9;
             this.label3.Text = "Major";
             // 
-            // comboBox1
+            // comboBoxMajor
             // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.comboBoxMajor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMajor.FormattingEnabled = true;
+            this.comboBoxMajor.Items.AddRange(new object[] {
             "CIS",
             "DS"});
-            this.comboBox1.Location = new System.Drawing.Point(63, 110);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 23);
-            this.comboBox1.TabIndex = 10;
+            this.comboBoxMajor.Location = new System.Drawing.Point(63, 110);
+            this.comboBoxMajor.Name = "comboBoxMajor";
+            this.comboBoxMajor.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxMajor.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "GPA";
+            // 
+            // textBoxGpa
+            // 
+            this.textBoxGpa.Location = new System.Drawing.Point(63, 139);
+            this.textBoxGpa.Name = "textBoxGpa";
+            this.textBoxGpa.Size = new System.Drawing.Size(121, 23);
+            this.textBoxGpa.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBoxMin);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBoxMax);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBoxGpax);
+            this.groupBox1.Location = new System.Drawing.Point(12, 197);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 175);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 101);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 15);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Min";
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(60, 98);
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.Size = new System.Drawing.Size(121, 23);
+            this.textBoxMin.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 15);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Max";
+            // 
+            // textBoxMax
+            // 
+            this.textBoxMax.Location = new System.Drawing.Point(60, 61);
+            this.textBoxMax.Name = "textBoxMax";
+            this.textBoxMax.Size = new System.Drawing.Size(121, 23);
+            this.textBoxMax.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 15);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "GPAx";
+            // 
+            // textBoxGpax
+            // 
+            this.textBoxGpax.Location = new System.Drawing.Point(60, 30);
+            this.textBoxGpax.Name = "textBoxGpax";
+            this.textBoxGpax.Size = new System.Drawing.Size(121, 23);
+            this.textBoxGpax.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxGpa);
+            this.Controls.Add(this.comboBoxMajor);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBoxname);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxid);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip2);
             this.Name = "Form1";
@@ -195,6 +286,8 @@
             this.menuStrip2.ResumeLayout(false);
             this.menuStrip2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -210,12 +303,21 @@
         private DataGridViewTextBoxColumn ColumnID;
         private DataGridViewTextBoxColumn ColumnName;
         private DataGridViewTextBoxColumn ColumnMajor;
-        private TextBox textBox2;
+        private TextBox textBoxid;
         private Button button1;
         private Label label1;
         private Label label2;
-        private TextBox textBox3;
+        private TextBox textBoxname;
         private Label label3;
-        private ComboBox comboBox1;
+        private ComboBox comboBoxMajor;
+        private Label label4;
+        private TextBox textBoxGpa;
+        private GroupBox groupBox1;
+        private Label label7;
+        private TextBox textBoxMin;
+        private Label label6;
+        private TextBox textBoxMax;
+        private Label label5;
+        private TextBox textBoxGpax;
     }
 }
--- Student Management/Student Management/Form1.cs
+++ Student Management/Student Management/Form1.cs
@@ -16,60 +16,59 @@ namespace Student_Management
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
-           
+
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "CSV (*.csv) | *.csv";
-            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
                 string[] readAllLine = File.ReadAllLines(openFileDialog.FileName);
-                string readAllText = File.ReadAllText(openFileDialog.FileName);
-                //this.textBox1.Text = readAllText;
-                //this.dataGridView1.Rows.Add()
 
-                for(int i = 0; i < readAllLine.Length; i++) {
+                for (int i = 0; i < readAllLine.Length; i++)
+                {
                     string studentRAW = readAllLine[i];
                     string[] studenSplied = studentRAW.Split(',');
                     Student student = new Student(studenSplied[0], studenSplied[1], studenSplied[2]);
                     //addDataToGridView(student);
                     //TODO: Add Student object to DataGridView
                 }
-                
+
+
             }
-            
+
         }
-        private void addDataToGridView(string id, string name, string major) {
+        private void addDataToGridView(string id, string name, string major)
+        {
             //this.dataGridView1.Rows.Add(new string[] { id, name, major });
         }
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
-        {   
-            //string data = this.dataGridView1.Rows[0].Cells[0].Value.ToString();
-            
-            string strData = string.Empty;
-            
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "CSV (*.csv) | *.csv";
-            if (saveFileDialog.ShowDialog() == DialogResult.OK) {
-                if(saveFileDialog.FileName != string.Empty) {
-                    
+        {
+            string strdata = string.Empty;
 
-                    int row = this.dataGridView1.Rows.Count;
-                    for (int i = 0; i < row; i++)
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV(*.csv) | *.csv";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (saveFileDialog.FileName != string.Empty)
+                {
+                    int column = this.dataGridView1.Rows.Count;
+                    int Row = 0;
+                    for (int i = 0; i < Row; i++)
                     {
-                        int column = this.dataGridView1.Columns.Count;
-                        for(int j = 0; j < column; j++) {
-                            strData = this.dataGridView1.Rows[i].Cells[j].Value.ToString();
-                            //TODO save data from dataGridView1 to variable
+
+                        for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                        {
+                            //strdata = this.dataGridView1.Rows[i].Cells[j].Value.ToString();
                         }
                     }
-
-                    
-                    //save flie
-                    File.WriteAllText(saveFileDialog.FileName, strData, Encoding.UTF8);
-
                 }
 
+                File.WriteAllText(saveFileDialog.FileName, strdata, Encoding.UTF8);
             }
         }
+
+       
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -78,7 +77,27 @@ namespace Student_Management
 
         private void button1_Click(object sender, EventArgs e)
         {
+            dataGridView1.Rows[0].Cells[0].Value = textBoxid.Text;
+            dataGridView1.Rows[0].Cells[1].Value = textBoxname.Text;
+            dataGridView1.Rows[0].Cells[2].Value = comboBoxMajor.Text;
 
+
+            Calculator calculator = new Calculator();
+            string input = textBoxGpa.Text;
+
+
+            double dInput = Convert.ToDouble(input);
+            calculator.addGPA(dInput, Name);
+
+            double gpax = calculator.getGPAx();
+            textBoxGpax.Text = gpax.ToString();
+
+            double max = calculator.getMax();
+            textBoxMax.Text = max.ToString();
+
+            double min = calculator.getMin();
+            textBoxMin.Text = min.ToString();
         }
     }
 }
+           
